f798f18bd4a8141d5a104b78868c618ecee0e3c4
dels11 <daniel.eelsner@gmail.com>
2013-11-03 14:54:11 -0600

    dragonboard code

---
+++ Android/src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	public int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void increment(View view){
+    	counter++;
+    	System.out.println(counter);
+    }
+    
+}
