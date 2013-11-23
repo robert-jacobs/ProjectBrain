1157125ab66f2cda90bcf043109ef46a3e23bfe5
zthomas910 <zthomas910@gmail.com>
2013-11-23 15:57:44 -0600

    attempt to fix iRobot Remote.  Still receiving illegalState Exception Error

--- IRobotRemote/src/com/example/irobotremote/MainActivity.java
+++ IRobotRemote/src/com/example/irobotremote/MainActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -39,7 +40,29 @@ public class MainActivity extends Activity {
 	BroadcastReceiver receiver;
 	String tag = "debugging";
 	
-	Handler mHandler;	
+	Handler mHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);			
+			switch(msg.what){
+			case SUCCESS_CONNECT:
+				ConnectedThread connectedThread = new ConnectedThread((BluetoothSocket)msg.obj);
+				
+				Log.i(tag, "Starting to write...NOT");
+				
+				Log.i(tag, "Going to read...NOT");
+				
+				break;
+			case MESSAGE_READ:
+				byte[] readBuf = (byte[])msg.obj;
+				String string = new String(readBuf);
+				Toast.makeText(getApplicationContext(), string, 0).show();
+				break;
+			}
+		}
+	};
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,7 +118,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void moveFoward() {
+	public void moveFoward(View view) {
 		mHandler = new Handler(){
 			@Override
 			public void handleMessage(Message msg) {
